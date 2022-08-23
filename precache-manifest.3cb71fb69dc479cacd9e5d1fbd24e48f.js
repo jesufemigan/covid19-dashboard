d982e195d7541efc5c75d8c9fd6735339ebb9429
ElpidaHope <paul.oladapo@yahoo.com.com>
2022-08-23 04:36:02 +0100

    Updates

---
+++ precache-manifest.3cb71fb69dc479cacd9e5d1fbd24e48f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2fa66836718d472a01c",
+    "url": "/covid19-dashboard/static/css/main.4ee1a94b.chunk.css"
+  },
+  {
+    "revision": "f2fa66836718d472a01c",
+    "url": "/covid19-dashboard/static/js/main.f2fa6683.chunk.js"
+  },
+  {
+    "revision": "7984912c93713be75d82",
+    "url": "/covid19-dashboard/static/js/1.7984912c.chunk.js"
+  },
+  {
+    "revision": "e3ed43d4adf2299cf04b",
+    "url": "/covid19-dashboard/static/js/2.e3ed43d4.chunk.js"
+  },
+  {
+    "revision": "4f99dc2ea87ad2f3f8c1",
+    "url": "/covid19-dashboard/static/js/runtime~main.4f99dc2e.js"
+  },
+  {
+    "revision": "91e9f4658523dd6b4c5ca90405d90e17",
+    "url": "/covid19-dashboard/index.html"
+  }
+];
